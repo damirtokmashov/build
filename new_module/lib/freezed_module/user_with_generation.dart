9041445cf3ec92c3ccecc056d859d17ef750c3a2
Damir Tokmashov <damir.tokmashov@gmail.com>
2021-04-10 23:29:32 +0600

    first commit

---
+++ new_module/lib/freezed_module/user_with_generation.dart
@@ -0,0 +1,11 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user_with_generation.freezed.dart';
+
+@freezed
+abstract class UserWithGeneration with _$UserWithGeneration {
+  const factory UserWithGeneration({
+    String name,
+    int age,
+  }) = _UserWithGeneration;
+}
